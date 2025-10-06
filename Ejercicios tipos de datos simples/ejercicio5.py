aabe6cf96f09404690b90877aacef9fa98b44efe
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-06 12:39:36 +0200

    ejercicio5

---
+++ Ejercicios tipos de datos simples/ejercicio5.py
@@ -0,0 +1,4 @@
+horas = int(input("Introduzca el número de horas trabajadas: "))
+coste = int(input("Introduzca el coste por hora: "))
+paga = horas * coste
+print("La paga que le corresponde es de:",paga)
