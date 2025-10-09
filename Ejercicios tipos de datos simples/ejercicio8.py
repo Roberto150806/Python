c781f8069accc745e03884952d8e2996e09429b8
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-09 10:29:51 +0200

    ejercicio8

---
+++ Ejercicios tipos de datos simples/ejercicio8.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Introduzca un numero: "))
+numero2 = int(input("Introduzca otro numero: "))
+
+cociente = numero1 // numero2
+resto = numero1 % numero2
+
+print(numero1, "entre" , numero2 , "da un cociente" , cociente , "y un resto" , resto)
