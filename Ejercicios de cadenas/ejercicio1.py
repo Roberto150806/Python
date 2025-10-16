7e611e1fdd4a56ede82f4ba13c1bb5918058299c
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-16 11:13:34 +0200

    ejercicio1cadenas

---
+++ Ejercicios de cadenas/ejercicio1.py
@@ -0,0 +1,7 @@
+nombre = input("Introduzca el nombre de usuario: ")
+numero = int(input("Introduzca un número entero: "))
+numerowhile = int(0)
+
+while numerowhile < numero:
+    numerowhile = numerowhile + 1
+    print(nombre)
