1dd777efe3301369e0d91e74b96c230e3fcbd5d0
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-16 09:42:20 +0200

    ejercicio10completo

--- Ejercicios tipos de datos simples/ejercicio10.py
+++ Ejercicios tipos de datos simples/ejercicio10.py
@@ -4,4 +4,6 @@ pesomuñeca = int(75)
 payasos = int(input("Introduzca el número de payasos vendidos: "))
 muñecas = int(input("Introduzca el número de muñecas vendidas: "))
 
-pesototal = (input)
+pesototal = pesopayaso * payasos + pesomuñeca * muñecas
+
+print("El peso total del paquete es de" ,pesototal, "g")
