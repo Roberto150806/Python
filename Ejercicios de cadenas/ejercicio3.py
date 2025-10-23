b7b476f08d40b226e1663a722abc1dd168ea3f60
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-23 09:49:56 +0200

    ejercicio3cadenas

---
+++ Ejercicios de cadenas/ejercicio3.py
@@ -0,0 +1,3 @@
+nombre = input("Introduzca su nombre de usuario: ")
+cantidadn = len(nombre)
+print(nombre,"tiene" ,cantidadn,"letras, donde" ,nombre.upper(), "es el nombre de usuario en mayúsculas y",cantidadn, "es el número de letras que tienen el nombre")
