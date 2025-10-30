083ef10802a803146f329d0d1eb4ca914f8bcd49
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-30 10:03:33 +0100

    ejercicio9cadenas

---
+++ Ejercicios de cadenas/ejercicio9.py
@@ -0,0 +1,7 @@
+nacimiento = input("Introduzca su fecha de nacimiento: ")
+
+dia = nacimiento.split("/")[0]
+mes = nacimiento.split("/")[1]
+año = nacimiento.split("/")[2]
+
+resultado = print("Nació el dia" ,dia, "del mes" ,mes, "y del año" ,año)
