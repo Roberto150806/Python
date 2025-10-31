67c4e04c630d04d2ae851e132e13159adf26e498
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-10-31 14:07:17 +0100

    ejercicio3condicionalescompleto

--- Ejercicios de Condicionales/ejercicio3.py
+++ Ejercicios de Condicionales/ejercicio3.py
@@ -1,9 +1,10 @@
 numero1 = int(input("Introduzca un numero: "))
 numero2 = int(input("Introduzca otro numero: "))
 
-division = numero1 / numero2
+
 
 if numero2 == 0:
     print("Error")
 else:
+    division = numero1 / numero2
     print (division)
