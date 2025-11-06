f67dc6c00881013a99ff6e60834f61ecd5bad351
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2025-11-06 09:52:53 +0100

    ejercicio5condicionalescompleto

--- Ejercicios de Condicionales/ejercicio5.py
+++ Ejercicios de Condicionales/ejercicio5.py
@@ -1,7 +1,7 @@
 edad = int(input("Introduzca su edad: "))
 ingresos = int(input("Introduzca sus ingresos mensuales: "))
 
-if edad < 16 & ingresos < 1000:
+if edad < 16 or ingresos < 1000:
     print("No debe tributar")
-elif edad > 16 & ingresos >= 1000:
+elif edad > 16 and ingresos >= 1000:
     print("Debe tributar")
