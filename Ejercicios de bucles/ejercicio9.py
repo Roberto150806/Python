2b23b94f71e1b77c69bc41aae79343b812103b4d
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2026-01-08 10:31:22 +0100

    ejercicio9bucles

---
+++ Ejercicios de bucles/ejercicio9.py
@@ -0,0 +1,6 @@
+contraseña = "hola"
+pregunta = input("Introduzca la contraseña:")
+
+while pregunta != contraseña:
+    print("Contraseña incorrecta. Pruebe de nuevo")
+    pregunta = input("Introduzca la contraseña: ")
