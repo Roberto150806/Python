e97be890c6084372ef7c45afd5f601cdf536903a
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2026-01-08 11:13:41 +0100

    ejercicio1corregidodiccionarios

--- Ejercicios de diccionarios/ejercicio1.py
+++ Ejercicios de diccionarios/ejercicio1.py
@@ -1,3 +1,7 @@
 diccionario =  {'Euro':'€','Dollar':'$', 'Yen':'¥'}
 pregunta = input("Introduzca una divisa: ")
-print(diccionario[pregunta])
+
+if pregunta in diccionario:
+    print(diccionario[pregunta])
+else:
+    print("La divisa no esta en el diccionario")
