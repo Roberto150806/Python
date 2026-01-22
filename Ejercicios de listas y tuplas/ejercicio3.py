fdda998f67500457dd7a654fd2847af28df6172a
Roberto Rodriguez <rrodzun1508@ieszaidinvergeles.org>
2026-01-22 11:02:43 +0100

    ejercicio3listasytuplas

---
+++ Ejercicios de listas y tuplas/ejercicio3.py
@@ -0,0 +1,5 @@
+asignaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
+
+for asignatura in asignaturas:
+    nota = int(input("Que nota has sacado en " +asignatura))
+    print("En" ,asignatura, "has sacado" ,nota)
